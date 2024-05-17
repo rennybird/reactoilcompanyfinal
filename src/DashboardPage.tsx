b1a04bb176fa3c203cec8237db59a1d9f4c0fa57
Supakorn Charamat <ohmsupakorn@gmail.com>
2024-05-18 05:05:12 +0700

    add db test

--- src/DashboardPage.tsx
+++ src/DashboardPage.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import './DashboardPage.css';  // Create this CSS file for custom styles
+import './DashboardPage.css';
 
 const DashboardPage: React.FC = () => {
+  const exportReport = async () => {
+    try {
+      const response = await fetch('https://oilcompany.azurewebsites.net/api/HttpTriggerPDF?code=5bOJ2pW-elC_fciTqWMTvlWBpf549zzME_qzn7kLfVqhAzFu7VdPLw%3D%3D', { method: 'POST' });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'report.pdf');
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <div className="dashboard-container flex flex-col items-center justify-center h-screen bg-gradient-to-r from-gray-700 via-gray-900 to-black text-white">
       <h1 className="text-5xl font-bold mb-8">PowerBI Financial Dashboard</h1>
+      <button onClick={exportReport} className="bg-blue-500 text-white py-2 px-4 rounded">Export as PDF</button>
       <div className="w-11/12 h-4/5 mt-8 border-4 border-yellow-400 rounded-lg overflow-hidden">
         <iframe
           title="try_finance"
--- src/WelcomePage.tsx
+++ src/WelcomePage.tsx
@@ -5,7 +5,7 @@ import './WelcomePage.css';  // Create this CSS file for custom styles
 const WelcomePage: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white">
-      <h1 className="text-6xl font-bold mb-8">Welcome to Our Dashboard</h1>
+      <h1 className="text-6xl font-bold mb-8">Welcome to TAS Dashboard</h1>
       <Link to="/dashboard">
         <button className="bg-white text-blue-700 font-bold py-2 px-8 rounded-full hover:bg-gray-200 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110">
           Go to Dashboard
