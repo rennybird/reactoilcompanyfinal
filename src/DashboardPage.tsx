485be79632618f3bbfdc5cbecbb242087c0d1112
Supakorn Charamat <ohmsupakorn@gmail.com>
2024-05-23 18:59:35 +0700

    re

--- src/DashboardPage.tsx
+++ src/DashboardPage.tsx
@@ -2,24 +2,6 @@ import React from 'react';
 import './DashboardPage.css';
 
 const DashboardPage: React.FC = () => {
-  const exportReport = async () => {
-    try {
-      const response = await fetch('https://oilcompany.azurewebsites.net/api/HttpTriggerPDF?code=5bOJ2pW-elC_fciTqWMTvlWBpf549zzME_qzn7kLfVqhAzFu7VdPLw%3D%3D', { method: 'POST' });
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const blob = await response.blob();
-      const url = window.URL.createObjectURL(blob);
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', 'report.pdf');
-      document.body.appendChild(link);
-      link.click();
-      link.remove();
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
 
   return (
     <div className="dashboard-container flex flex-col items-center justify-center h-screen bg-gradient-to-r from-gray-700 via-gray-900 to-black text-white">
